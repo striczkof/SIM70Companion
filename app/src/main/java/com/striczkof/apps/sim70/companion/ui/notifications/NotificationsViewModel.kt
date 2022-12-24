a24bab7e97d4b1646f04cf2d06df23921b64176d
striczkof <alvin.a@striczkof.com>
2022-12-25 00:03:23 +1100

    Icon setup and others plus loicense

---
+++ app/src/main/java/com/striczkof/apps/sim70/companion/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.striczkof.apps.sim70.companion.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
